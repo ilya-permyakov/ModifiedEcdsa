8dc9d59a9ee4669d01708a34f6a998daa72d82ce
Илья Пермяков <ilyaperm2018@yandex.ru>
2024-04-27 22:51:07 +0300

    удалил лишний закомментированный код

--- CurveConfig.py
+++ CurveConfig.py
@@ -4,33 +4,6 @@ import timeit
 
 
 class GenCurveConfig:
-
-    """
-    @staticmethod
-    def is_prime(n, k=15):
-        if n % 2 == 0:
-            return False
-
-        d = n - 1
-        s = 0
-
-        while d % 2 == 0:
-            d //= 2
-            s += 1
-
-        for _ in range(k):
-            a = secrets.randbelow(n - 2) + 2
-            b = pow(a, d, n)
-            if b == 1 or b == n - 1:
-                continue
-            for _ in range(s - 1):
-                b = (b * b) % n
-                if b == n - 1:
-                    break
-            else:
-                return False
-        return True
-    """
     @staticmethod
     def generate_prime():
         start_time = timeit.default_timer()
