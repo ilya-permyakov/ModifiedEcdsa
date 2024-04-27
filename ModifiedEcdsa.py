cc458c6006a6ea59936b075eeb0a825456495715
Илья Пермяков <ilyaperm2018@yandex.ru>
2024-04-27 21:37:22 +0300

    доделал приложение, теперь для каждого этапа открывается окно и можно подписывать файлы

--- ModifiedEcdsa.py
+++ ModifiedEcdsa.py
@@ -17,28 +17,37 @@ class ModifiedECDSA:
         num = int(hex_dig, 16)
         return num
 
+    @staticmethod
+    def hash_file_to_int(filename):
+        with open(filename, 'rb') as f:
+            file_content = f.read()
+        hash_obj = sha3_256(file_content)
+        hex_dig = hash_obj.hexdigest()
+        num = int(hex_dig, 16)
+        return num
+
     def gen_keys(self):
         d = secrets.randbelow(self.subgroup_order - 1) + 1
         Q = self.base_point.mult(self.base_point, pow(d, -1, self.subgroup_order))
         return {'d': d, 'Q': Q}
 
-    def gen_sign(self, keys, message):
+    def gen_sign(self, keys, file):
         start_time = timeit.default_timer()
         k = secrets.randbelow(self.subgroup_order - 1) + 1
         P = self.base_point.mult(self.base_point, k)
         r = P.x % self.subgroup_order
-        s = (keys['d'] * (k - self.hash_string_to_int(message))) % self.subgroup_order
+        s = (keys['d'] * (k - self.hash_file_to_int(file))) % self.subgroup_order
         end_time = timeit.default_timer()
         print(f"Время генерации подписи: {end_time - start_time} секунд.")
         return {'r': r, 's': s}
 
-    def verification(self, message, sign, public_key):
+    def verification(self, file, sign, public_key):
         start_time = timeit.default_timer()
         if self.subgroup_order < sign['r'] < 1 and self.subgroup_order < sign['s'] < 1:
             result = 'Подпись неверна'
             return result
         u1 = sign['s'] % self.subgroup_order
-        u2 = self.hash_string_to_int(message) % self.subgroup_order
+        u2 = self.hash_file_to_int(file) % self.subgroup_order
         A = Point.add(public_key.mult(public_key, u1), self.base_point.mult(self.base_point, u2))
         if sign['r'] == A.x % self.subgroup_order:
             result = 'Подпись верна'
--- main.py
+++ main.py
@@ -116,8 +116,9 @@ class GenParams(QtWidgets.QMainWindow):
         ecdsa = ModifiedECDSA(self.params, self.G)
         self.keys = ecdsa.gen_keys()
 
-        self.Parameters.setText(f"a = {str(self.params['a'])}\nb = {str(self.params['b'])}\np = {str(self.params['p'])}\n\n"
-                                f"Базовая точка:\nx = {str(self.G['base_point'].x)}\ny = {str(self.G['base_point'].y)}\n\n"
+        self.Parameters.setText(f"a = {str(self.params['a'])}\nb = {str(self.params['b'])}\n"
+                                f"p = {str(self.params['p'])}\n\n"f"Базовая точка:\nx = {str(self.G['base_point'].x)}\n"
+                                f"y = {str(self.G['base_point'].y)}\n\n"
                                 f"Порядок подгруппы: {str(self.G['subgroup_order'])}")
         self.PrivateKeyLine.setText(str(self.keys['d']))
         self.PublicKeyLine.setText(f"x = {str(self.keys['Q'].x)}\ny = {str(self.keys['Q'].y)}")
@@ -177,10 +178,12 @@ class GenSign(QtWidgets.QMainWindow):
             'd': int(self.InputPrivateKeyLine.text())
         }
 
-        message = self.InputMessage.toPlainText()
+        file = self.InputFile.text()
+
+        # message = self.InputMessage.toPlainText()
 
         ecdsa = ModifiedECDSA(curve, G)
-        sign = ecdsa.gen_sign(private_key, message)
+        sign = ecdsa.gen_sign(private_key, file)
 
         self.YourSign.setText(f"r = {str(sign['r'])}\ns = {str(sign['s'])}")
 
@@ -197,6 +200,57 @@ class GenSign(QtWidgets.QMainWindow):
         self.InputPrivateKeyLine.setText(str(keys['d']))
 
 
+class VerifySign(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(VerifySign, self).__init__()
+        uic.loadUi('VerifySignWindow.ui', self)
+        self.setWindowTitle('Verify sign')
+        self.Verify.clicked.connect(self.verify_sign)
+        self.File.clicked.connect(self.open_file_dialog)
+
+    def open_file_dialog(self):
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
+        if file_name:
+            self.InputFile.setText(file_name)
+
+    def verify_sign(self):
+        base_point_text = self.InputBasePointVerify.toPlainText()
+        x, y = map(int, base_point_text.split('\n'))
+        base_point = Point(x, y)
+
+        sign = self.InputSignVerify.toPlainText()
+        r, s = map(int, sign.split('\n'))
+
+        curve = {
+            'a': int(self.InputAVerify.text()),
+            'b': int(self.InputBVerify.text()),
+            'p': int(self.InputPVerify.text())
+        }
+
+        public_key_text = self.InputPublicKeyVerify.toPlainText()
+        x, y = map(int, public_key_text.split('\n'))
+        public_key = Point(x, y, curve)
+
+        G = {
+            'base_point': base_point,
+            'subgroup_order': int(self.InputSubgroupOrderVerify.text())
+        }
+
+        sign_to_verify = {
+            'r': r,
+            's': s
+        }
+
+        file = self.InputFile.text()
+
+        # message = self.InputMessageVerify.toPlainText()
+
+        ecdsa = ModifiedECDSA(curve, G)
+        result = ecdsa.verification(file, sign_to_verify, public_key)
+
+        self.ResultVerify.setText(result)
+
+
 class MenuWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MenuWindow, self).__init__()
@@ -204,6 +258,7 @@ class MenuWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Menu")
         self.GenParams.clicked.connect(self.open_gen_param_window)
         self.GenSign.clicked.connect(self.open_gen_sign_window)
+        self.VerifySign.clicked.connect(self.open_verify_sign_window)
 
     def open_gen_param_window(self):
         self.gen_param_window = GenParams()
@@ -215,6 +270,10 @@ class MenuWindow(QtWidgets.QMainWindow):
         if data:
             self.gen_sign_window.set_data(data)
         self.gen_sign_window.show()
+
+    def open_verify_sign_window(self):
+        self.verify_sign_window = VerifySign()
+        self.verify_sign_window.show()
 
 
 app = QtWidgets.QApplication([])
