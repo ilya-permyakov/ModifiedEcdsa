df3796a9c90e02b91db3b00f371289758fe9dd9c
Илья Пермяков <ilyaperm2018@yandex.ru>
2024-05-09 19:46:38 +0300

    переделал оконное приложение, теперь там есть вкладки вместо окон

--- PlotCurve.py
+++ PlotCurve.py
@@ -1,14 +1,75 @@
+import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
+matplotlib.use('TkAgg')
 
-def plot_curve(curve_params):
-    p = curve_params['p']
-    a = curve_params['a']
-    b = curve_params['b']
+
+def plot_elliptic_curve(a, b, p):
+    x_vals = []
+    y_vals = []
 
     for x in range(p):
         for y in range(p):
-            if (y ** 2 - x ** 3 - a * x - b) % p == 0:
-                plt.scatter(x, y, s=1)
+            if (y * y - x * x * x - a * x - b) % p == 0:
+                x_vals.append(x)
+                y_vals.append(y)
+
+    plt.scatter(x_vals, y_vals)
+    plt.show()
+
+
+def plot_torus(R=1, r=0.3):
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, 2 * np.pi, 100)
+    u, v = np.meshgrid(u, v)
+
+    x = (R + r * np.cos(v)) * np.cos(u)
+    y = (R + r * np.cos(v)) * np.sin(u)
+    z = r * np.sin(v)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121, projection='3d')
+    ax1.set_zlim(-3, 3)
+    ax1.plot_surface(x, y, z, rstride=5, cstride=5, color='k', edgecolors='w')
+    plt.show()
+
+
+def plot_elliptic_curve_on_torus(a, b, p, R=1, r=0.2):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Generate torus
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, 2 * np.pi, 100)
+    u, v = np.meshgrid(u, v)
+    x = (R + r * np.cos(v)) * np.cos(u)
+    y = (R + r * np.cos(v)) * np.sin(u)
+    z = r * np.sin(v)
+    ax.plot_surface(x, y, z, color='b', alpha=0.3)
+
+    # Generate points on elliptic curve
+    theta_vals = []
+    phi_vals = []
+    for x in range(p):
+        for y in range(p):
+            if (y * y - x * x * x - a * x - b) % p == 0:
+                theta = 2 * np.pi * x / p
+                phi = 2 * np.pi * y / p
+                theta_vals.append(theta)
+                phi_vals.append(phi)
+
+    # Map points to torus
+    x_vals = (R + r * np.cos(phi_vals)) * np.cos(theta_vals)
+    y_vals = (R + r * np.cos(phi_vals)) * np.sin(theta_vals)
+    z_vals = r * np.sin(phi_vals)
+    ax.scatter(x_vals, y_vals, z_vals, color='r')
 
     plt.show()
+
+
+plot_elliptic_curve_on_torus(-3, 3, 13)
+
+# plot_torus()
+
+# plot_elliptic_curve(-3, 3, 13)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PyQt5 import uic, QtWidgets, QtCore
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtCore import QObject, pyqtSignal
 import sys
 from ModifiedEcdsa import ModifiedECDSA
 from BasePoint import BasePoint
@@ -6,34 +7,40 @@ from CurveConfig import GenCurveConfig
 from Point import Point
 
 
-class GenParams(QtWidgets.QMainWindow):
-    data_signal = QtCore.pyqtSignal(dict)
-
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(GenParams, self).__init__()
-        uic.loadUi('GenParamsWindow.ui', self)
-        self.setWindowTitle('Generate parameters')
-        self.GenParams.clicked.connect(self.generate_params)
-        self.ShowPrivateKey.clicked.connect(self.toggle_visibility)
+        super(MainWindow, self).__init__()
+        uic.loadUi('NewMainWindow.ui', self)
+        self.setWindowTitle("Цифровая подпись")
+        self.GenParams.clicked.connect(self.gen_params)
+        self.ShowPrivateKey.clicked.connect(self.show_pr_key)
+        self.UseGenParams.stateChanged.connect(self.fill_gen_sign_fields)
+        self.FileGenSign.clicked.connect(self.open_file_dialog)
+        self.FileVerifySign.clicked.connect(self.open_file_dialog)
+        self.Sign.clicked.connect(self.create_sign)
+        self.Verify.clicked.connect(self.verify_sign)
 
-    def generate_params(self):
-        self.params = GenCurveConfig().generate_params()
-        self.G = BasePoint(self.params).find_base_point()
-        ecdsa = ModifiedECDSA(self.params, self.G)
-        self.keys = ecdsa.gen_keys()
+    def gen_params(self):
+        params = GenCurveConfig().generate_params()
+        G = BasePoint(params).find_base_point()
+        ecdsa = ModifiedECDSA(params, G)
+        keys = ecdsa.gen_keys()
 
-        self.Parameters.setText(f"a = {str(self.params['a'])}\nb = {str(self.params['b'])}\n"
-                                f"p = {str(self.params['p'])}\n\n"f"Базовая точка:\nx = {str(self.G['base_point'].x)}\n"
-                                f"y = {str(self.G['base_point'].y)}\n\n"
-                                f"Порядок подгруппы: {str(self.G['subgroup_order'])}")
-        self.PrivateKeyLine.setText(str(self.keys['d']))
-        self.PublicKeyLine.setText(f"x = {str(self.keys['Q'].x)}\ny = {str(self.keys['Q'].y)}")
+        self.fill_gen_params_fields(params, G, keys)
 
         if self.UseGenParams.isChecked():
-            data = {"params": self.params, "G": self.G, "keys": self.keys}
-            self.data_signal.emit(data)
+            data = {"params": params, "G": G, "keys": keys}
+            self.fill_gen_sign_fields(data)
 
-    def toggle_visibility(self):
+    def fill_gen_params_fields(self, params, G, keys):
+        self.Parameters.setText(f"a = {str(params['a'])}\nb = {str(params['b'])}\n"
+                                f"p = {str(params['p'])}\n\n"f"Базовая точка:\nx = {str(G['base_point'].x)}\n"
+                                f"y = {str(G['base_point'].y)}\n\n"
+                                f"Порядок подгруппы: {str(G['subgroup_order'])}")
+        self.PrivateKeyLine.setText(str(keys['d']))
+        self.PublicKeyLine.setText(f"x = {str(keys['Q'].x)}\ny = {str(keys['Q'].y)}")
+
+    def show_pr_key(self):
         if self.PrivateKeyLine.echoMode() == QtWidgets.QLineEdit.Normal:
             self.PrivateKeyLine.setEchoMode(QtWidgets.QLineEdit.Password)
             self.ShowPrivateKey.setText("Показать")
@@ -41,28 +48,23 @@ class GenParams(QtWidgets.QMainWindow):
             self.PrivateKeyLine.setEchoMode(QtWidgets.QLineEdit.Normal)
             self.ShowPrivateKey.setText("Скрыть")
 
-    def open_gen_sign_window(self):
-        self.gen_sign_window = GenSign()
-
-        if self.UseGenParams.isChecked():
-            data = {"params": self.params, "G": self.G, "keys": self.keys}
-            self.gen_sign_window.set_data(data)
-
-        self.gen_sign_window.show()
-
-
-class GenSign(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(GenSign, self).__init__()
-        uic.loadUi('GenSignWindow.ui', self)
-        self.setWindowTitle('Generate sign')
-        self.Sign.clicked.connect(self.create_sign)
-        self.File.clicked.connect(self.open_file_dialog)
+    def fill_gen_sign_fields(self, data=None):
+        if self.UseGenParams.isChecked() and data is not None:
+            self.InputA.setText(str(data['params']['a']))
+            self.InputB.setText(str(data['params']['b']))
+            self.InputP.setText(str(data['params']['p']))
+            self.InputBasePoint.setPlainText(f"{str(data['G']['base_point'].x)}\n{str(data['G']['base_point'].y)}")
+            self.InputSubgroupOrder.setText(str(data['G']['subgroup_order']))
+            self.InputPrivateKeyLine.setText(str(data['keys']['d']))
 
     def open_file_dialog(self):
         file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
         if file_name:
-            self.InputFile.setText(file_name)
+            sender = self.sender()
+            if sender == self.FileGenSign:
+                self.InputFile.setText(file_name)
+            elif sender == self.FileVerifySign:
+                self.InputFileVerifySign.setText(file_name)
 
     def create_sign(self):
         base_point_text = self.InputBasePoint.toPlainText()
@@ -86,38 +88,10 @@ class GenSign(QtWidgets.QMainWindow):
 
         file = self.InputFile.text()
 
-        # message = self.InputMessage.toPlainText()
-
         ecdsa = ModifiedECDSA(curve, G)
         sign = ecdsa.gen_sign(private_key, file)
 
         self.YourSign.setText(f"r = {str(sign['r'])}\ns = {str(sign['s'])}")
-
-    def set_data(self, data):
-        params = data["params"]
-        G = data["G"]
-        keys = data["keys"]
-
-        self.InputA.setText(str(params['a']))
-        self.InputB.setText(str(params['b']))
-        self.InputP.setText(str(params['p']))
-        self.InputBasePoint.setPlainText(f"{str(G['base_point'].x)}\n{str(G['base_point'].y)}")
-        self.InputSubgroupOrder.setText(str(G['subgroup_order']))
-        self.InputPrivateKeyLine.setText(str(keys['d']))
-
-
-class VerifySign(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(VerifySign, self).__init__()
-        uic.loadUi('VerifySignWindow.ui', self)
-        self.setWindowTitle('Verify sign')
-        self.Verify.clicked.connect(self.verify_sign)
-        self.File.clicked.connect(self.open_file_dialog)
-
-    def open_file_dialog(self):
-        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
-        if file_name:
-            self.InputFile.setText(file_name)
 
     def verify_sign(self):
         base_point_text = self.InputBasePointVerify.toPlainText()
@@ -147,9 +121,7 @@ class VerifySign(QtWidgets.QMainWindow):
             's': s
         }
 
-        file = self.InputFile.text()
-
-        # message = self.InputMessageVerify.toPlainText()
+        file = self.InputFileVerifySign.text()
 
         ecdsa = ModifiedECDSA(curve, G)
         result = ecdsa.verification(file, sign_to_verify, public_key)
@@ -157,32 +129,7 @@ class VerifySign(QtWidgets.QMainWindow):
         self.ResultVerify.setText(result)
 
 
-class MenuWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(MenuWindow, self).__init__()
-        uic.loadUi('Menu.ui', self)
-        self.setWindowTitle("Menu")
-        self.GenParams.clicked.connect(self.open_gen_param_window)
-        self.GenSign.clicked.connect(self.open_gen_sign_window)
-        self.VerifySign.clicked.connect(self.open_verify_sign_window)
-
-    def open_gen_param_window(self):
-        self.gen_param_window = GenParams()
-        self.gen_param_window.data_signal.connect(self.open_gen_sign_window)
-        self.gen_param_window.show()
-
-    def open_gen_sign_window(self, data=None):
-        self.gen_sign_window = GenSign()
-        if data:
-            self.gen_sign_window.set_data(data)
-        self.gen_sign_window.show()
-
-    def open_verify_sign_window(self):
-        self.verify_sign_window = VerifySign()
-        self.verify_sign_window.show()
-
-
 app = QtWidgets.QApplication([])
-window = MenuWindow()
+window = MainWindow()
 window.show()
-sys.exit(app.exec())
+sys.exit(app.exec_())
